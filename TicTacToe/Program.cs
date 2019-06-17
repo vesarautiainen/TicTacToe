fdf31e6a140b14d92bc0d3167bed8465bb0e7c14
Vesa Rautiainen <verautia@microsoft.com>
2019-06-17 10:35:57 +0300

    Change dimension to 2.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,7 +4,7 @@ namespace TicTacToe
 {
     class Program
     {
-        static int dimension = 3;
+        static int dimension = 2;
         private static char emptyCell = ' ';
 
         static void Main(string[] args)
